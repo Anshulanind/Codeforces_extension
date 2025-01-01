af87e01ea3bcff31df21be1bb47b4b22e55a4e7b
Anshul Anand <anshulanind@gmail.com>
2025-01-02 03:50:32 +0530

    Update popup2.js

--- dist/popup2.js
+++ dist/popup2.js
@@ -1,14 +1,6 @@
 
 window.onload = function() {
-  const firebaseConfig = {
-    apiKey: "AIzaSyDWrpv9hYPMgI4qyZR5RDcL01Ijcu9HCuo",
-    authDomain: "codeforces-extension-5ff85.firebaseapp.com",
-    projectId: "codeforces-extension-5ff85",
-    storageBucket: "codeforces-extension-5ff85.firebasestorage.app",
-    messagingSenderId: "26786845982",
-    appId: "1:26786845982:web:1e2c9c11cf4cce56b2650f",
-    measurementId: "G-H2C2P4TEXP"
-  };
+  
 
   firebase.initializeApp(firebaseConfig);
   var db = firebase.database();
